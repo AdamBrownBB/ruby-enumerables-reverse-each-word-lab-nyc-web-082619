b5c97b4cc04120d08afaf2ca1d40ca969d074fb8
Adam Brown <adam.brown.betty@gmail.com>
2019-08-21 22:52:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,3 +15,13 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end  
+
+arr = (1..5).to_a        #=> [1, 2, 3, 4, 5]
+
+doubles = []
+arr.each { |x| doubles.push 2*x }
+
+# the two lines above do the same as this one:
+doubles = arr.collect { |x| 2*x }
+
+doubles                  #=> [2, 4, 6, 8, 10]
